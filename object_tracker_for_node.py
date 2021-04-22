7b7a61d86a35ec423277c28147dc622322c72e23
Thomas Boffy <thomasboffy68@gmail.com>
2021-04-22 17:56:36 +0200

    wip wip

--- object_tracker_for_node.py
+++ object_tracker_for_node.py
@@ -1,26 +1,177 @@
+import os
+# comment out below line to enable tensorflow logging outputs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import time
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from absl import app, flags, logging
+from absl.flags import FLAGS
+import core.utils as utils
+from core.yolov4 import filter_boxes
+from tensorflow.python.saved_model import tag_constants
+from core.config import cfg
+import cv2
+import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+# deepsort
+from deep_sort import preprocessing, nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
 import sys
 import json
 
 
-def read_in():
-    line = sys.stdin.readline()
-    # Since our input would only be having one line, parse our JSON data from that
-    return json.loads(line)
+flags.DEFINE_string('weights', cfg.PATH+'/checkpoints/yolov4-tiny-416', 'path to weights file')
+flags.DEFINE_integer('size', 416, 'resize images to')
+flags.DEFINE_boolean('tiny', True, 'yolo or yolo-tiny')
+flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+flags.DEFINE_float('iou', 0.45, 'iou threshold')
+flags.DEFINE_float('score', 0.50, 'score threshold')
 
 
-def main():
+def main(_argv):
+    # Definition of the parameters
+    max_cosine_distance = 0.4
+    nn_budget = None
+    nms_max_overlap = 1.0
+
+    # initialize deep sort
+    model_filename = cfg.PATH+'/model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    # calculate cosine distance metric
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    # initialize tracker
+    tracker = Tracker(metric)
+
+    # load configuration for object detector
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    input_size = FLAGS.size
+
+    # tf
+    saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']
+
     while True:
-        data = read_in()
-        image = data['frame_image']
-        shape = data['frame_shape']
-        id = data['frame_id']
-        print(json.dumps({
-            'frame_id': id,
-            'result': {
-                'fps': 0,
-                'tracks': []
-            }
-        }, indent=4))
+        data = sys.stdin.readline()
+        if data:
+            data = json.loads(data)
+            if data['end']:
+                break
+            id = data['frame_id']
+            frame = cv2.imdecode(np.array(data['frame_image'], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            frame_size = frame.shape[:2]
+            image_data = cv2.resize(frame, (input_size, input_size))
+            image_data = image_data / 255.
+            image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+            # tf
+            batch_data = tf.constant(image_data)
+            pred_bbox = infer(batch_data)
+            for key, value in pred_bbox.items():
+                boxes = value[:, :, 0:4]
+                pred_conf = value[:, :, 4:]
+
+            boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+                boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+                scores=tf.reshape(
+                    pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+                max_output_size_per_class=50,
+                max_total_size=50,
+                iou_threshold=FLAGS.iou,
+                score_threshold=FLAGS.score
+            )
+
+            # convert data to numpy arrays and slice out unused elements
+            num_objects = valid_detections.numpy()[0]
+            bboxes = boxes.numpy()[0]
+            bboxes = bboxes[0:int(num_objects)]
+            scores = scores.numpy()[0]
+            scores = scores[0:int(num_objects)]
+            classes = classes.numpy()[0]
+            classes = classes[0:int(num_objects)]
+
+            # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, width, height
+            original_h, original_w, _ = frame.shape
+            bboxes = utils.format_boxes(bboxes, original_h, original_w)
+
+            # store all predictions in one parameter for simplicity when calling functions
+            pred_bbox = [bboxes, scores, classes, num_objects]
+
+            # read in all class names from config
+            class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+            # by default allow all classes in .names file
+            allowed_classes = list(class_names.values())
+
+            # custom allowed classes (uncomment line below to customize tracker for only people)
+            #allowed_classes = ['person']
+
+            # loop through objects and use class index to get class name, allow only classes in allowed_classes list
+            names = []
+            deleted_indx = []
+            for i in range(num_objects):
+                class_indx = int(classes[i])
+                class_name = class_names[class_indx]
+                if class_name not in allowed_classes:
+                    deleted_indx.append(i)
+                else:
+                    names.append(class_name)
+            names = np.array(names)
+            count = len(names)
+
+            # delete detections that are not in allowed_classes
+            bboxes = np.delete(bboxes, deleted_indx, axis=0)
+            scores = np.delete(scores, deleted_indx, axis=0)
+
+            # encode yolo detections and feed to tracker
+            features = encoder(frame, bboxes)
+            detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+
+            # run non-maxima supression
+            boxs = np.array([d.tlwh for d in detections])
+            scores = np.array([d.confidence for d in detections])
+            classes = np.array([d.class_name for d in detections])
+            indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+            detections = [detections[i] for i in indices]
+
+            # Call the tracker
+            tracker.predict()
+            tracker.update(detections)
+
+            # Store tracks for json...
+            tracks = []
+
+            # update tracks
+            for track in tracker.tracks:
+                if not track.is_confirmed() or track.time_since_update > 1:
+                    continue
+                bbox = track.to_tlbr()
+                class_name = track.get_class()
+                t = dict()
+                t["class"] = class_name
+                t["bbox"] = bbox.tolist()
+                t["id"] = track.track_id
+                t["confidence"] = track.detection_actual_score
+                tracks.append(t)
+
+            # send data to Node!
+            print(json.dumps({
+                'frame_id': id,
+                'result': tracks
+            }))
+
 
 if __name__ == '__main__':
-    main()
+    try:
+        app.run(main)
+    except SystemExit:
+        pass

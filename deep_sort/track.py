2082aa34caa3843fd2f6ae21f358684b8503fb8e
Thomas Boffy <thomasboffy68@gmail.com>
2021-05-19 10:50:13 +0200

    fix score format

--- deep_sort/track.py
+++ deep_sort/track.py
@@ -143,7 +143,7 @@ class Track:
         self.mean, self.covariance = kf.update(
             self.mean, self.covariance, detection.to_xyah())
         self.features.append(detection.feature)
-        self.detection_actual_score = format(detection.confidence, '.2f')
+        self.detection_actual_score = detection.confidence
 
         self.hits += 1
         self.time_since_update = 0
